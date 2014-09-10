3125f59b653163e73acc87a7c5d93d8469470063
Aaron Flower <aaronpflower@gmail.com>
2014-09-10 15:14:29 -0400

    First working admin show page and tests

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -8,4 +8,8 @@ class AdminsController < ApplicationController
 
 	def new 
 	end
+
+	def create
+	end
+	
 end
--- spec/requests/admin_pages_spec.rb
+++ spec/requests/admin_pages_spec.rb
@@ -5,9 +5,10 @@ describe "Admin pages" do
 	subject { page }
 
 	describe "admin page" do
-		before { visit admin_path }
+		let(:admin) { FactoryGirl.create(:admin) }
+		before { visit admin_path(admin) }
 
-		it { should have_content('Admin') }
+		it { should have_content(admin.email) }
 		it { should have_title('Admin') }
 	end
 end
