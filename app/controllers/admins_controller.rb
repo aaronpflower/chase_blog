11f145eaff11b5af345428631d7bfd080822de04
Aaron Flower <aaronpflower@gmail.com>
2014-09-10 14:22:07 -0400

    Controller now renders show view

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,11 @@
 class AdminsController < ApplicationController
 
 	def show
+		@admin = Admin.find(params[:id])
+		# this needs to route to the post creation page.  Which is only
+		# accessed through sessions and can be seen as the "profile page"
 	end
 
+	def new 
+	end
 end
